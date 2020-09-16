8c6eec504a695397286a54da02596f41da9deb7c
yubin <yubin890816@163.com>
2020-09-16 23:43:44 +0800

    Eureka Server高可用集群部署

---
+++ eureka-server2/src/test/java/com/yubin/springcloud/eurekaserver/EurekaServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yubin.springcloud.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
